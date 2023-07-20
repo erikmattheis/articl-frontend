033279db9f10d8467e7c5b7c7e5ff15e5d870172
erikmattheis <gozz@gozz.com>
2023-07-20 16:37:30 -0500

    Correction:use articlType, not tyep

--- src/components/layout/ArticlTypeTabs.vue
+++ src/components/layout/ArticlTypeTabs.vue
@@ -1,10 +1,11 @@
 <template>
-  <ul class="nav-tabs nav-tabs-inner-margin">slug:{{ slug }}<br>articlType:{{ articlType }}
+  <ul class="nav-tabs nav-tabs-inner-margin">slug:{{ slug }}<br>articlType:{{ articlType }}<br>articlTypes:{{ articlTypes
+  }}
     <li v-for="type in articlTypes" :key="type" :class="{ active: type === articlType }">
       <a
         href="#"
-        @click.prevent="$router.push({ name: 'ResourceIndex', params: { slug, type } })"
-        @keyup.enter.prevent="$router.push({ name: 'ResourceIndex', params: { slug, type } })">
+        @click.prevent="$router.push({ name: 'ResourceIndex', params: { slug, articlType: type } })"
+        @keyup.enter.prevent="$router.push({ name: 'ResourceIndex', params: { slug, articlType: type } })">
         {{ type }}</a>
     </li>
   </ul>
