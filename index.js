b3cc82d496afb9d59f547c98ef9c57927e847c95
erikmattheis <gozz@gozz.com>
2023-08-07 01:13:50 -0500

    add history mode

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import compression from 'compression';
 import serveStatic from 'serve-static';
+import history from 'connect-history-api-fallback';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
@@ -8,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 
+app.use(history());  
 app.use(compression());
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
