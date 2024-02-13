4b958eb3e6e949dc823f021078a7b311a6500566
Erik Mattheis <erik@mattheis.org>
2024-02-12 20:30:24 -0600

    Change backend url

--- src/services/axiosService.js
+++ src/services/axiosService.js
@@ -4,8 +4,8 @@ let baseURL = "";
 
 if (window.location.hostname === "192.168.1.130" || window.location.hostname === "localhost") {
   baseURL = "http://localhost:5000/v1";
-} else if (window.location.hostname === "articl-vue-2022.herokuapp.com") {
-  baseURL = "https://articl-restful-api.herokuapp.com/v1";
+} else if (window.location.hostname === "articl-frontend.herokuapp.com" || window.location.hostname === "articl.net") {
+  baseURL = "https://articl-backend.herokuapp.com/v1";
 }
 export default axios.create({
   baseURL,
