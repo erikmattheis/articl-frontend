a0aa5a7324517d1d541ead3bb4831b78383dcbd2
naveenk22 <64447713+naveenk22@users.noreply.github.com>
2024-07-06 21:04:39 -0500

    Update ArticlTypeTabs.vue
    Modified changeArticlType

--- src/components/layout/ArticlTypeTabs.vue
+++ src/components/layout/ArticlTypeTabs.vue
@@ -39,13 +39,10 @@ export default {
     }),
   },
   methods: {
-    changeArticlType(articlType) {
-      //this.$store.dispatch("resources/articlType", articlType);
-
-      this.$router.push({ name: 'ArticlsList', params: { slug: this.slug, articlType } });
-
-
-    },
+     changeArticlType(articlType) {
+    this.$store.dispatch("resources/setArticlType", articlType);
+    this.$router.push({ name: 'ArticlsList', params: { slug: this.slug, articlType } });
+  },
   },
 };
 
