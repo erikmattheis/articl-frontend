62080a39b85de753fab81c75235dd16f06fb77ba
erikmattheis <erik@mattheis.org>
2024-08-29 17:52:05 -0500

    prerender

--- prerender.js
+++ prerender.js
@@ -3,37 +3,38 @@ const httpStatus = require("http-status");
 const { Categories } = require("../models");
 const { getCategorySlugNames } = require("./categories.service");
 const ApiError = require("../utils/ApiError");
-
-
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const path = require("path");
 
 const prerender = async (n = 3) => {
   try {
     const categories = await getCategorySlugNames(n);
-    console.log('num categories:', categories.length);
+    console.log("num categories:", categories.length);
     const result = await renderPages(categories);
-
+    console.log("Pages rendered successfully");
+  } catch (err) {
+    console.log("err:", err);
   }
-  catch (err) {
-    console.log('err:', err);
-  }
-}
+};
 
 const renderPages = async (categories) => {
+  const browser = await puppeteer.launch();
   const promises = categories.map(async (category) => {
     const { slug, name } = category;
-    const result = await getAISummary('covid-19', name);
-    console.log('result:', result);
+    const page = await browser.newPage();
+    await page.goto(`${config.baseUrl}/category/${slug}`, {
+      waitUntil: "networkidle2",
+    });
+    const content = await page.content();
+    const filePath = path.join(__dirname, `../prerendered/${slug}.html`);
+    fs.writeFileSync(filePath, content);
+    await page.close();
   });
-  return Promise.all(promises);
-}
-
-/*
-setTimeout(async function() {
-  const f = await getAISummary('covid-19', 'infectuous disease');
-  //console.log('f:', f);
-}, 1000);
-*/
+  await Promise.all(promises);
+  await browser.close();
+};
 
 module.exports = {
-  prerender
-}
+  prerender,
+};
